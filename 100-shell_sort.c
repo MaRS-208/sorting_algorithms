e475b1c04051adce67d4e892f243eb5da41aa5af
MaRS-208 <manuela.riossosa@gmail.com>
2022-06-13 23:45:33 -0300

    commit

---
+++ 100-shell_sort.c
@@ -0,0 +1,14 @@
+#include "sort.h"
+
+/**
+ * shell_sort - sorts an array of integers in ascending order\
+   using the Shell sort algorithm, using the Knuth sequence
+ * @array: array of numbers
+ * @size: size of array
+ * Return: 0
+ */
+
+void shell_sort(int *array, size_t size)
+{
+	pass;
+}
