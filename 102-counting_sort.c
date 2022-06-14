86d7382e2d79d412facea994526b6f896dd79a5d
MaRS-208 <manuela.riossosa@gmail.com>
2022-06-14 00:00:42 -0300

    commit

---
+++ 102-counting_sort.c
@@ -0,0 +1,14 @@
+#include "sort.h"
+
+/**
+ * counting_sort - sorts an array of integers in ascending order\
+   using the Shell sort algorithm, using the Knuth sequence
+ * @array: array of numbers
+ * @size: size of array
+ * Return: 0
+ */
+void counting_sort(int *array, size_t size)
+{
+	pass;
+}
+
