39e82f77311d41a6e41b166794dd46e308d58bba
MaRS-208 <manuela.riossosa@gmail.com>
2022-06-13 23:56:21 -0300

    commit

---
+++ 101-cocktail_sort_list.c
@@ -0,0 +1,14 @@
+#include "sort.h"
+
+/**
+ * cocktail_sort_list - sorts a doubly linked list of integers\
+ in ascending order using the Cocktail shaker sort algorithm
+ * @array: array of numbers
+ * @size: size of array
+ * Return: 0
+ */
+
+void cocktail_sort_list(listint_t **list)
+{
+	pass;
+}
